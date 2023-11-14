2e3d4784b8e86361f890d842f8a7a75bb39ed1f2
darshil929 <darshilt929@gmail.com>
2023-11-14 14:48:01 +0530

    generic_helper.py and save_to_db fn

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import Request
 import generic_helper
+import db_helper
 
 app = FastAPI()
 
@@ -24,3 +25,19 @@ async def handle_request(request: Request):
 
     return intent_handler_dict[intent](parameters, session_id)
 
+def save_to_db(order: dict):
+    next_order_id = db_helper.get_next_order_id()
+
+    for food_item, quantity in order.items():
+        rcode = db_helper.insert_order_item(
+            food_item,
+            quantity,
+            next_order_id
+        )
+
+        if rcode == -1:
+            return -1
+
+    db_helper.insert_order_tracking(next_order_id, "in progress")
+
+    return next_order_id
