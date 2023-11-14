5210a1f8987bf3713ef51f110a92deac958690a9
darshil929 <darshilt929@gmail.com>
2023-11-14 14:40:26 +0530

    handle request fn

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi import Request
+import generic_helper
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+inprogress_orders = {}
+
+@app.post("/")
+async def handle_request(request: Request):
+    payload = await request.json()
+
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+    session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
+
+    intent_handler_dict = {
+        'order.add - context: ongoing-order': add_to_order,
+        'order.remove - context: ongoing-order': remove_from_order,
+        'order.complete - context: ongoing-order': complete_order,
+        'track.order - context: ongoing-tracking': track_order
+    }
+
+    return intent_handler_dict[intent](parameters, session_id)
 
